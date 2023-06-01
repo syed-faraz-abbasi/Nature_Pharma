6aa183d890d10580e3b8a40c420844061d44b132
Imran <116140391+imransharda@users.noreply.github.com>
2023-06-01 15:07:36 +0530

    home page

--- registration/src/components/home.js
+++ registration/src/components/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import {
-    Col, Container, Row,
+    Col, Container, NavLink, Row,
 
 
 
@@ -12,12 +12,13 @@ import {
 
 import 'react-slideshow-image/dist/styles.css';
 import icon1 from '../components/images/icon1.png';
-import angryimg from '../components/images/angryimg.png';
-import userIm from '../components/images/Landing-Page.png';
-import testimonial from '../components/images/testimonial.jpg';
-
+import userIm from '../components/images/about-1.jpeg';
+import pharmacist from '../components/images/pharmacist.png';
 
 import person from '../components/images/person.jpg';
+
+import slidebg from '../components/images/2.png';
+
 
 
 
@@ -42,7 +43,10 @@ const Home = ({ location }) => {
                                             <button className='btn_b'>Know More</button>
                                         </Col>
                                         <Col md={6}>
-                                            <img src={person} className='img4' alt='img' />
+                                            <div className="slider-img">
+                                                <img src={person} className='img1' alt='img' />
+                                                <img src={slidebg} className='img2' alt='img' />
+                                            </div>    
                                         </Col>
                                     </Row>
                                     </Container>
@@ -57,7 +61,10 @@ const Home = ({ location }) => {
                                             <button className='btn_b'>Know More</button>
                                         </Col>
                                         <Col md={6}>
-                                            <img src={person} className='img4' alt='img' />
+                                        <div className="slider-img">
+                                                <img src={person} className='img1' alt='img' />
+                                                <img src={slidebg} className='img2' alt='img' />
+                                            </div>  
                                         </Col>
                                     </Row>
                                     </Container>
@@ -67,13 +74,16 @@ const Home = ({ location }) => {
                                     <Container>
                                     <Row>
                                         <Col md={6}>
-                                            <h1>Welcome to <br />Sharda Pharmacy</h1>
+                                            <h1>Welcome to  Sharda Pharmacy</h1>
                                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />Mauris laoreet ante quis faucibus commodo. Aenean pharetra arcu a augue euismod,</p>
                                             <button className='btn_p'>Read More</button>
                                             <button className='btn_b'>Know More</button>
                                         </Col>
                                         <Col md={6}>
-                                            <img src={person} className='img4' alt='img' />
+                                        <div className="slider-img">
+                                                <img src={person} className='img1' alt='img' />
+                                                <img src={slidebg} className='img2' alt='img' />
+                                            </div>  
                                         </Col>
                                     </Row>
                                     </Container>
@@ -84,18 +94,40 @@ const Home = ({ location }) => {
                     </Row>
                 </Container>
             </div>
+            <section className="bandblue">
+                <Container>
+                    <Row>
+                        <Col md={2}>
+                        </Col>
+
+                         
+                        <Col md={8}> 
+                            <div className="talkto-pharmasist">
+                            <img src={pharmacist} alt='img' />
+
+                            <h4>Talk to Our
+                            <strong>Pharmacist</strong>
+                            </h4>
+                            <NavLink className='button1' href="#">View Details</NavLink>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
             <div className='home-about'>
                 <Container>
                     <Row>
-
                         <Col md={6}>
                             <img src={userIm} alt='img' />
                         </Col>
-                        <Col md={6} >
+                        <Col md={6}>
                             <div className='home-content'>
-                                <h1>Welcome to Sharda Pharmacy</h1>
-                                <p>You and your loved ones’ health come first. With high-quality medications and supplies provided by skilled and experienced pharmacists, you can guarantee to live a healthy life within your budget. </p>
-                                <a href="https://example.com">Link</a>
+                                <h2>Welcome to 
+                                    <strong>Sharda Pharmacy </strong></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id dui sed nulla blandit blandit eu et leo. Nunc auctor volutpat libero in interdum. Maecenas quis enim lorem. Donec in felis lorem. Proin sit amet enim ante. Vivamus mollis pharetra elit, non luctus risus aliquam id. Phasellus a imperdiet enim. Morbi varius mauris non leo aliquam eleifend. Nullam at pulvinar eros. Proin ac posuere dui. Cras feugiat egestas libero, et volutpat lorem tincidunt eget. Morbi luctus, mi in congue venenatis, ex dolor ullamcorper lectus, at congue felis ipsum nec dui.
+<br /> <br /> 
+Curabitur nec nisl in sem scelerisque blandit id nec diam. Suspendisse placerat pellentesque mauris sed consectetur. Nullam ut justo porta, porttitor orci id, pulvinar tortor. Sed in orci vulputate, egestas enim quis, convallis libero. Praesent ultrices enim sit amet porta tempor. Sed id mauris eros. In hac habitasse platea dictumst. Curabitur fringilla dictum scelerisque.</p>
+                                <NavLink className='button1' href="#">Read More </NavLink>
                             </div>
                         </Col>
 
@@ -103,138 +135,67 @@ const Home = ({ location }) => {
                 </Container>
             </div>
 
-            <div className="center-excellence">
-                <div className="container">
-                    <div className="row">
-                        <div className="hedings col-md-12">
-                            <h3 className="text-center pb-3">Services We Offer</h3>
-                        </div>
-                        <div className="centerofexcellence-slider arrowsldier">
-
-
-                            <div className="ce-item">
-                                <div className="global">
-
-                                    <div className="img-wrap">
-                                        <img src={icon1} alt='img' className=' ' />
-                                    </div>
-                                    <div className="head-wrap">
-                                        <h4>Institute of Dental Sciences</h4>
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div className="ce-item">
-                                <div className="global">
-
-                                    <div className="img-wrap">
-                                        <img src={icon1} className=' ' alt='img' />
-                                    </div>
-                                    <div className="head-wrap">
-                                        <h4>Institute of Orthopedic & Spine</h4>
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div className="ce-item">
-                                <div className="global">
-
-                                    <div className="img-wrap">
-                                        <img src={icon1} className=' ' alt='img' />
-                                    </div>
-                                    <div className="head-wrap">
-                                        <h4>Institute of Neuro Sciences</h4>
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div className="ce-item">
-                                <div className="global">
-
-                                    <div className="img-wrap">
-                                        <img src={icon1} className=' ' alt='img' />
-                                    </div>
-                                    <div className="head-wrap">
-                                        <h4>Institute of Renal Sciences & Transplant</h4>
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div className="ce-item">
-                                <div className="global">
-
-                                    <div className="img-wrap">
-                                        <img src={icon1} className=' ' alt='img' />
-                                    </div>
-                                    <div className="head-wrap">
-                                        <h4>Institute of Oncology </h4>
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div className="ce-item">
-                                <div className="global">
-
-                                    <div className="img-wrap">
-                                        <img src={icon1} className=' ' alt='img' />
-                                    </div>
-                                    <div className="head-wrap">
-                                        <h4>Institute of Critical Care</h4>
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div className="ce-item">
-                                <div className="global">
-
-                                    <div className="img-wrap">
-                                        <img src={icon1} className=' ' alt='img' />
-                                    </div>
-                                    <div className="head-wrap">
-                                        <h4>Institute of Heart </h4>
-                                    </div>
-
-                                </div>
-                            </div>
-
-
-
-
-
-                        </div>
-                        <div className="view-wrap">
-
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='home-about fold3'>
-                {/* <Container> */}
+            <div className="home-services">
+                <Container>
                     <Row>
-
-                        <Col md={5}>
-                            <img src={testimonial} className='testimonial' alt='img' />
+                        <Col md={12}>
+                            <h2>Services <strong>We Offer </strong></h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id dui sed nulla blandit blandit eu et leo. Nunc auctor volutpat libero in interdum.</p>
                         </Col>
-                        <Col md={7} >
-                            <div className='home-test'>
-
-                                <p>“We choose to specialise in Avada <br />
-                                    because it is both functional and <br />
-                                    practical, and  our clients simply love it! It has stood the test of time and <br />
-                                    continues to evolve – an excellent <br />
-                                    solution for our talented design team, <br />
-                                    who continue to push the boundaries.” </p>
-
+                    </Row> 
+                    <Row>
+                        <Col md={3}>
+                             <div className="services-in">
+                                <div className="img-wrap">
+                                    <img src={icon1} alt='img' className=' ' />
+                                </div>
+                                <div className="contant">
+                                    <h4>Pharmacy </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                                    <NavLink href='#' className='button1'>Read More</NavLink>
+                                </div>
                             </div>
-                        </Col>
-
+                         </Col>
+                         <Col md={3}>
+                             <div className="services-in">
+                                <div className="img-wrap">
+                                    <img src={icon1} alt='img' className=' ' />
+                                </div>
+                                <div className="contant">
+                                    <h4>Pharmacy </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                                    <NavLink href='#' className='button1'>Read More</NavLink>
+                                </div>
+                            </div>
+                         </Col>
+                         <Col md={3}>
+                             <div className="services-in">
+                                <div className="img-wrap">
+                                    <img src={icon1} alt='img' className=' ' />
+                                </div>
+                                <div className="contant">
+                                    <h4>Pharmacy </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                                    <NavLink href='#' className='button1'>Read More</NavLink>
+                                </div>
+                            </div>
+                         </Col>
+                         <Col md={3}>
+                             <div className="services-in">
+                                <div className="img-wrap">
+                                    <img src={icon1} alt='img' className=' ' />
+                                </div>
+                                <div className="contant">
+                                    <h4>Pharmacy </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                                    <NavLink href='#' className='button1'>Read More</NavLink>
+                                </div>
+                            </div>
+                         </Col>
                     </Row>
-                {/* </Container> */}
-                
-
-
+                </Container>
             </div>
+            
             
             
 
