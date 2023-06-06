514cdc940bce06db0a5ea48f4da4fe139a1bf9de
Imran <116140391+imransharda@users.noreply.github.com>
2023-06-06 15:25:41 +0530

    home

--- registration/src/components/about.js
+++ registration/src/components/about.js
@@ -11,16 +11,13 @@ import {
 // import Header from './header';
 
 import 'react-slideshow-image/dist/styles.css';
-import icon1 from '../components/images/icon1.png';
-import userIm from '../components/images/about-1.jpeg';
-import pharmacist from '../components/images/pharmacist.png';
+ 
 
 import person from '../components/images/person.jpg';
 
 import slidebg from '../components/images/2.png';
 
-import Footer from './footer';
-
+ 
 
 
 const Home = ({ location }) => {
@@ -29,57 +26,35 @@ const Home = ({ location }) => {
     return (
         
         <>
-             <div className='slider'>
-                <Container fluid>
+             <div className='slider slider-in'>
+                <Container>
                     <Row>
-                        <Col>
-                            <Slide>
-
-                                 
-                                <div className="slidein">
-                                    <Container>
-                                    <Row>
-                                        <Col md={6}>
-                                            <h1>About</h1>
+                             <Col md={12}>
+                               <h1>About</h1>
                                              
-                                        </Col>
-                                        <Col md={6}>
-                                        <div className="slider-img">
-                                                <img src={person} className='img1' alt='img' />
-                                                <img src={slidebg} className='img2' alt='img' />
-                                            </div>  
-                                        </Col>
-                                    </Row>
-                                    </Container>
-
-                                </div>
-                                 
-
-                            </Slide>
-                        </Col>
-                    </Row>
+                            </Col>
+                     </Row>
                 </Container>
             </div>
-            <br/>
+            <div className='coming-tet'>
+                <Container>
+                    <Row>
+                             <Col md={12}>
+                             <br/><br/>
                 <br/>
+                 
                 <h1>About Coming Soon</h1>
                 <br/>
                 <br/>
                 <br/>
                 <br/><br/><br/><br/><br/><br/><br/>
                 <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-
-
-            
-            
-
-
+                
+                                             
+                            </Col>
+                     </Row>
+                </Container>
+            </div>
         </>
 
 
--- registration/src/components/events.js
+++ registration/src/components/events.js
@@ -26,54 +26,35 @@ const Home = ({ location }) => {
     return (
         
         <>
-              <div className='slider'>
-                <Container fluid>
+               <div className='slider slider-in'>
+                <Container>
                     <Row>
-                        <Col>
-                            <Slide>
-
-                                 
-                                <div className="slidein">
-                                    <Container>
-                                    <Row>
-                                        <Col md={6}>
-                                            <h1>Events</h1>
+                             <Col md={12}>
+                               <h1>Events</h1>
                                              
-                                        </Col>
-                                        <Col md={6}>
-                                        <div className="slider-img">
-                                                <img src={person} className='img1' alt='img' />
-                                                <img src={slidebg} className='img2' alt='img' />
-                                            </div>  
-                                        </Col>
-                                    </Row>
-                                    </Container>
-
-                                </div>
-                                 
-
-                            </Slide>
-                        </Col>
-                    </Row>
+                            </Col>
+                     </Row>
                 </Container>
             </div>
-            <br/>
+            <div className='coming-tet'>
+                <Container>
+                    <Row>
+                             <Col md={12}>
+                             <br/><br/>
                 <br/>
-                <h1>Event Coming Soon</h1>
+                 
+                <h1>Coming Soon</h1>
                 <br/>
                 <br/>
                 <br/>
                 <br/><br/><br/><br/><br/><br/><br/>
                 <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-
-
-            
+                
+                                             
+                            </Col>
+                     </Row>
+                </Container>
+            </div>
             
 
 
