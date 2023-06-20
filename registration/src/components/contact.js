654564ebf5fffac2e278eae924d1ab6aff8b0958
Faraz6222 <faraz.abbasi@shardatech.org>
2023-06-20 10:57:20 +0530

    contactus

--- registration/src/components/contact.js
+++ registration/src/components/contact.js
@@ -42,7 +42,7 @@ const Home = ({ location }) => {
                 <div className="wrapper">
 
                     <main>
-                        <div id="post-11" className="post-11 page type-page status-publish hentry">
+                        <div id="post-11" >
                             <div className="entry-content">
                                 <p>Please don't hesitate to talk to us regarding our services. A team representative will get back to you as soon as possible to answer your concern.</p>
                                 <p>You may send us a direct message using the form below.</p>
@@ -50,16 +50,16 @@ const Home = ({ location }) => {
                         </div>
                     </main>
                 </div>
-                <div  className="template_form">
-
-
+                <div  className="template_form ">
+                    <Container>
                     <form id="submitform" name="contact" method="post" encType="multipart/form-data" action="">
-                        <span className="required-info">* Required Information</span>
-                        <div className="form_box1">
+                          Required Information
+                        <span>*</span>
+                        <div className="form_box">
                             <div className="form_label">
                                 <label className="text_uppercase">
-                                    <strong>Full Name <span className="required_filed">*</span></strong>
-                                </label>
+                                    <strong>Full Name <span>*</span></strong>
+                                </label><br/>
                             </div>
                             <div className="">
                                 <div className="field_holder animated_box">
@@ -73,8 +73,8 @@ const Home = ({ location }) => {
 
 
                             <label className="text_uppercase">
-                                <strong>Address <span className="required_filed">*</span></strong>
-                            </label>
+                                <strong>Address <span>*</span></strong>
+                            </label><br/>
 
 
                             <input type="text" name="Address" className="form_field" value="" id="Address" placeholder="Enter address here" />
@@ -86,28 +86,19 @@ const Home = ({ location }) => {
 
 
                             <label className="text_uppercase">
-                                <strong>Email Address <span className="required_filed">*</span></strong>
-                            </label>
+                                <strong>Email Address <span >*</span></strong>
+                            </label><br/>
 
 
                             <input type="text" name="Email_Address" className="form_field" value="" id="Email_Address" placeholder="Enter email address here" />
-
-
-                            <label className="text_uppercase">
-                                <strong>Phone <span className="required_filed">*</span></strong>
-                            </label>
-
-
-                            <input type="text" name="Phone" className="form_field" value="" id="Phone" placeholder="Enter phone here" onKeyPress="return isNumberKey(evt)" />
-
                         </div>
 
 
                         <div className="form_box">
 
                             <label className="text_uppercase">
-                                <strong>Question / Comment <span className="required_filed"></span></strong>
-                            </label>
+                                <strong>Question / Comment </strong>
+                            </label><br/>
 
 
                             <textarea name="Question_or_Comment" className="text form_field" id="Question_or_Comment" placeholder="Enter your question or comment here"></textarea>
@@ -122,6 +113,10 @@ const Home = ({ location }) => {
                         </div>
 
                     </form>
+                    </Container>
+
+
+                    
 
 
                 </div>
